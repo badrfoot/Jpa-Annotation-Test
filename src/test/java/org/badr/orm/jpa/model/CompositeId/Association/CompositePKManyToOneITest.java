4af760f9958b5a40726ea05b049b678ea60a9b0e
Oussama BADR <oussama.badr@holydis.com>
2017-05-17 15:40:24 +0200

    + Either Use Flush or Commit for ShouldPersistEntitiesForCompositeKeyWithAssociation_OK

--- src/test/java/org/badr/orm/jpa/model/CompositeId/Association/CompositePKManyToOneITest.java
+++ src/test/java/org/badr/orm/jpa/model/CompositeId/Association/CompositePKManyToOneITest.java
@@ -21,11 +21,11 @@ import org.slf4j.LoggerFactory;
  *
  * @author OBD
  */
-public class JpaITest extends BaseClassITest {
+public class CompositePKManyToOneITest extends BaseClassITest {
 
-	Logger LOGGER = LoggerFactory.getLogger(JpaITest.class);
+	Logger LOGGER = LoggerFactory.getLogger(CompositePKManyToOneITest.class);
 
-	public JpaITest() {
+	public CompositePKManyToOneITest() {
 	}
 
 	@BeforeClass
@@ -51,7 +51,7 @@ public class JpaITest extends BaseClassITest {
 	 * Method should pass but we have errors see hibernate Bug for Hibernate 5.0_
 	 * <a href=https://hibernate.atlassian.net/browse/HHH-10623>https://hibernate.atlassian.net/browse/HHH-10623</a>
 	 */
-	@Test //@Ignore
+	@Test  @Ignore
 	public void ShouldPersistEntitiesForCompositeKeyWithAssociation_NotOk() {
 
 		Department department1 = new Department("IT", "Seatle");
@@ -89,11 +89,7 @@ public class JpaITest extends BaseClassITest {
 		assertEquals(employee1, employee2);
 	 }
 
-
-	/**
-	 * Method should pass but we have errors see hibernate Bug for Hibernate 5.0_
-	 * <a href=https://hibernate.atlassian.net/browse/HHH-10623>https://hibernate.atlassian.net/browse/HHH-10623</a>
-	 */
+	
 	@Test //@Ignore
 	public void ShouldPersistEntitiesForCompositeKeyWithAssociation_OK() {
 
@@ -104,7 +100,12 @@ public class JpaITest extends BaseClassITest {
 
 		transaction.begin();
 		entityManager.persist(department1);
+
+		// Either persist or flush as you like...
+		LOGGER.debug("Persist Department...");
+		//entityManager.flush();
 		transaction.commit();
+		LOGGER.debug("Persist Department...OK");
 
 		// If we comment below line ==> the test pass whithout
 		entityManager.detach(department1);
@@ -113,13 +114,17 @@ public class JpaITest extends BaseClassITest {
 					(department1 == null), (employee1.getDepartment() == null));
 		LOGGER.debug( "Outside Transaction - employee1.toString() = {}", employee1.toString());
 
+		// You should comment below line If you 've used flush for previous perist
 		transaction.begin();
+		
 		LOGGER.debug( "Inside  Transaction - Before Persist employee1.toString() = {}", employee1.toString());
 		LOGGER.debug( "Inside Transaction - Before Persist IsNull(department1) = {} - IsNull(employee1.getDepartment()) = {}",
 					(department1 == null), (employee1.getDepartment() == null));
 		LOGGER.debug( "Insisde Transaction - Before Persist employee1.toString() = {}", employee1.toString());
+
 		employee1.setDepartment(entityManager.find(Department.class, department1.getId()));
 		entityManager.persist(employee1);
+
 		LOGGER.debug( "Inside Transaction - After Persist IsNull(department1) = {} - IsNull(employee1.getDepartment()) = {}",
 					(department1 == null), (employee1.getDepartment() == null));
 		LOGGER.debug( "Insisde Transaction - After Persist employee1.toString() = {}", employee1.toString());
